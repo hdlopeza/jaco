6f3a2a05c30ab65dfc0083740d2a45344127a1bd
hernan <hernand.lopeza@gmail.com>
2021-04-14 22:37:45 -0500

    antes

--- vision/ejemplo2/eje24.py
+++ vision/ejemplo2/eje24.py
@@ -1,4 +1,5 @@
 
+#%%
 import os
 import re
 import cv2
@@ -7,14 +8,17 @@ import numpy as np
 import pandas as pd
 
 #! HYPERPARAMETROS
-ERODE_ITERATIONS = 0 #1
-DILATE_ITERATIONS = 3 #6
-HIGHT_MAX = 40 # 40
-HIGHT_MIN = 9 # 16
-THRESHOLD_COLUMN = 12 #29
-THRESHOLD_ROW = 2 #29
 
-def mostrar(titulo, image):
+# ERODE_ITERATIONS, DILATE_ITERATIONS, HIGHT_MAX, HIGHT_MIN, THRESHOLD_COLUMN, THRESHOLD_ROW = [1, 6, 40, 16, 29, 29]
+ERODE_ITERATIONS, DILATE_ITERATIONS, HIGHT_MAX, HIGHT_MIN, THRESHOLD_COLUMN, THRESHOLD_ROW = [0, 6, 40, 9, 20, 3]
+
+def mostrar(titulo, image, boxess):
+
+    for box in boxess:
+        (x, y, w, h) = box
+        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 1)
+
+
     # Using cv2.imshow() method 
     # Displaying the image 
     cv2.imshow(titulo, image)
@@ -26,7 +30,23 @@ def mostrar(titulo, image):
     #closing all open windows 
     cv2.destroyAllWindows() 
 
+def mostrar1(titulo, image):
+    # Using cv2.imshow() method 
+    # Displaying the image 
+    cv2.imshow(titulo, image)
+    
+    #waits for user to press any key 
+    #(this is necessary to avoid Python kernel form crashing)
+    cv2.waitKey(0) 
+    
+    #closing all open windows 
+    cv2.destroyAllWindows() 
+
+
+
+# in_file = os.path.join("data", "test3.png")
 in_file = os.path.join("data", "test2.png")
+
 
 img = cv2.imread(os.path.join(in_file))
 # img = img[785:1150, 70:1620] # Fraccion de la imagen
@@ -37,14 +57,14 @@ imgx = img.copy()
 img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 img = cv2.GaussianBlur(img, (3, 3), 0)
 img = cv2.threshold(img, 250, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
-mostrar('1', img)
+# mostrar('1', img, [(0,0,0,0)])
 img1 = cv2.bitwise_not(img)
-mostrar('2', img1)
+# mostrar('2', img1, [(0,0,0,0)])
 struct = cv2.getStructuringElement(cv2.MORPH_RECT, (2, 2))
 img1 = cv2.erode(img1, struct, iterations=ERODE_ITERATIONS)
-mostrar('3', img1)
+# mostrar('3', img1, [(0,0,0,0)])
 img1 = cv2.dilate(img1, struct, anchor=(-1, -1), iterations=DILATE_ITERATIONS)
-mostrar('4', img1)
+# mostrar('4', img1, [(0,0,0,0)])
 contours, hierarchy = cv2.findContours(img1, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 boxes = []
 for contour in contours:
@@ -54,29 +74,16 @@ for contour in contours:
     if HIGHT_MIN < h < HIGHT_MAX:
         boxes.append(box)
 
-#----------
-_img = imgx.copy()
-for box in boxes:
-    (x, y, w, h) = box
-    cv2.rectangle(_img, (x, y), (x + w - 2, y + h - 2), (0, 255, 0), 1)
-mostrar('5', _img)
-#----------
+# mostrar('5', imgx.copy(), boxes)
 
-boxes = list(sorted(boxes, key=lambda r: (r[1], r[0])))
+boxes = list(sorted(boxes, key=lambda r: (r[0], r[1])))
 rows = {}
 for box in boxes:
     (x, y, w, h) = box
     row_key = y // 10
     rows[row_key] = [box] if row_key not in rows else rows[row_key] + [box]
 
-
-#----------
-_img = imgx.copy()
-for box in boxes:
-    (x, y, w, h) = box
-    cv2.rectangle(_img, (x, y), (x + w - 2, y + h - 2), (0, 255, 0), 1)
-mostrar('6', _img)
-#----------
+# mostrar('6', imgx.copy(), boxes)
 
 boxes = []
 htemp = []
@@ -95,15 +102,7 @@ for i in rows:
             boxes[-1] = (x3, y3, w2+w3 + (x2 - (x1 + w1)), h2)
         htemp.append(box)
 
-
-#----------
-_img = imgx.copy()
-for box in boxes:
-    (x, y, w, h) = box
-    cv2.rectangle(_img, (x, y), (x + w - 2, y + h - 2), (0, 255, 0), 1)
-mostrar('7', _img)
-#----------
-
+# mostrar('7', imgx.copy(), boxes)
 
 boxes = list(sorted(boxes, key=lambda r: (str(r[0])[:2], r[1])))
 cols = {}
@@ -129,25 +128,15 @@ for i in cols:
             boxes[-1] = (x2, y1, w1, h1+h2)
         htemp.append(box)
 
-#----------
-_img = imgx.copy()
-for box in boxes:
-    (x, y, w, h) = box
-    cv2.rectangle(_img, (x, y), (x + w, y + h), (0, 0, 255), 1)
+# mostrar('8', imgx.copy(), boxes)
 
-mostrar('8', _img)
-#----------
-
-
-# boxes1 = list(sorted(boxes, key=lambda r: (r[1], r[0])))
+boxes = list(sorted(boxes, key=lambda r: (r[1], r[0])))
 cols1 = {}
 for box in boxes:
     (x, y, w, h) = box
     row_key = x // 10
     cols1[row_key] = [box] if row_key not in cols1 else cols1[row_key] + [box]
-
 # cols1
-
 
 punto = []
 for i in cols1:
@@ -221,34 +210,41 @@ for line in punto_columna:
     y2 = punto_fila[-1]
     cv2.line(_img, (x1, y1), (x2, y2), (0, 0, 255), 1)
 
-mostrar('9', _img)
+mostrar('9', _img, [(0,0,0,0)])
 #----------
 
 
 # celda = np.empty((len(punto_fila) - 1,len(punto_columna) - 1), dtype='object')
-# # celdax = np.empty((len(punto_fila),len(punto_columna)), dtype='object')
+# celdax = np.empty((len(punto_fila) - 1,len(punto_columna) - 1), dtype='object')
 # for i in range(len(punto_fila)):
 #     for ii in range(len(punto_columna)):
 #         try:
 #             text = re.sub(r"[\n]|[\x0c]|[,]", "", 
-#                           pytesseract.image_to_string(
-#                               imgx[punto_fila[i]-3:punto_fila[i+1], 
-#                                    punto_columna[ii]:punto_columna[ii+1]],
-#                               config='--psm 6'))
+#                           pytesseract.image_to_string(imgx[punto_fila[i]-3:punto_fila[i+1], punto_columna[ii]:punto_columna[ii+1]], config='--psm 6'))
 #             celda[i, ii] = text
-#             # celdax[i, ii] = (punto_fila[i],punto_fila[i+1], '--' , punto_columna[ii],punto_columna[ii+1])
+#             celdax[i, ii] = (punto_fila[i],punto_fila[i+1], '--' , punto_columna[ii],punto_columna[ii+1])
 #         except:
 #             pass
 
 # pd.DataFrame(celda)
-
-# mostrar(imgx)
-
+# pd.DataFrame(celdax)
 
 
-# result = []
-# for box in boxes:
-#     result.append(pytesseract.image_to_string(imgx[
-#         box[1]-1:box[1]+box[3], box[0]:box[0]+box[2]]))
-# result
+#%%
+img = imgx[22:43, 357:424]
 
+img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+img = cv2.GaussianBlur(img, (3, 3), 0)
+img = cv2.threshold(img, 250, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+# img = cv2.bitwise_not(img)
+struct = cv2.getStructuringElement(cv2.MORPH_RECT, (1, 1))
+img = cv2.erode(img, struct, iterations=ERODE_ITERATIONS)
+img = cv2.dilate(img, struct, anchor=(-1, -1), iterations=DILATE_ITERATIONS)
+
+
+
+mostrar1('prueba', img)
+pytesseract.image_to_string(im, config='--psm 6')
+
+
+# %%
