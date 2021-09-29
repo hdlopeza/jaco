cbb5d7cc91cbc90dd90712627b27505bad066477
hernan <hernand.lopeza@gmail.com>
2021-09-28 22:16:37 -0500

    rancheras

--- vision/ejemplo1/convert_pdf.py
+++ vision/ejemplo1/convert_pdf.py
@@ -1,3 +1,5 @@
+"Convierte un documento de pdf a jpg"
+
 # conda install -c conda-forge poppler
 # pip install pillow
 
@@ -20,19 +22,19 @@ with BytesIO() as f:
     img_page = Image.open(f)
 
 image = Image.open(out_file)
-alto_fijo = 1024
-alto_porcentaje = (alto_fijo / float(image.size[1]))
-ancho_objetivo = int(float(image.size[0])* float(alto_porcentaje))
-image_new = image.resize((ancho_objetivo, alto_fijo))
-image_new.save("hh.jpg")
+# alto_fijo = 1024
+# alto_porcentaje = (alto_fijo / float(image.size[1]))
+# ancho_objetivo = int(float(image.size[0])* float(alto_porcentaje))
+# image_new = image.resize((ancho_objetivo, alto_fijo))
+# image_new.save("hh.jpg")
 
 ancho_fijo = 768
 ancho_porcentaje = (ancho_fijo / float(image.size[0]))
 alto_objetivo = int(float(image.size[1]) * float(ancho_porcentaje))
 image_new = image.resize((ancho_fijo, alto_objetivo))
-image_new.save("hh1.jpg")
+# image_new.save("hh1.jpg")
 
-pages[0].save("hh1.jpg", format="JPEG")
+pages[0].save(out_file, format="JPEG")
 
-plotting = plt.imshow(img_bin)
+plotting = plt.imshow(pages[0])
 plt.show()
