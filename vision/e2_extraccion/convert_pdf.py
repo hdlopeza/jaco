c7872fb2bd8c77af3ab9bcef48ebeb59b8babe55
hdlopeza <hernand.lopeza@gmail.com>
2021-11-12 18:20:23 -0500

    inicio de consolidacion

--- vision/e2_extraccion/convert_pdf.py
+++ vision/e2_extraccion/convert_pdf.py
@@ -4,54 +4,92 @@
 # pip install pillow
 # conda install -c conda-forge pdf2image
 
-#%%
+# %%
 import os
 import cv2
-from pdf2image import convert_from_path
-# import matplotlib.pyplot as plt
+import numpy as np
 
+from pdf2image import (convert_from_path, convert_from_bytes)
+
+# import matplotlib.pyplot as plt
 # from io import BytesIO
 # from PIL import Image
 
-#%%
+def pdf_a_imagen(folder, file):
 
-in_file = os.path.join('../', "data", r'8.pdf')
-out_file = os.path.join('../', "data", r'8_out.jpg')
-pages = convert_from_path(in_file)
-pages[0].save(out_file, format="JPEG")
+    _path = os.path.splitext(file)
 
-# pages[0]
+    _in = os.path.join(folder, file)
+    _out = os.path.join(folder, _path[0] + '_new.jpg')
+
+    pages = convert_from_bytes(pdf_file=open(_in, 'rb').read())
+    image_new = np.asarray(pages[0])
+
+    ancho_fijo = 768
+    alto, ancho, canales = image_new.shape
+    ancho_porcentaje = (ancho_fijo / ancho)
+    alto_objetivo = int(float(alto) * float(ancho_porcentaje))
+    image_new = cv2.resize(image_new, (ancho_fijo, alto_objetivo))
+    cv2.imwrite(_out, image_new)
+
+
+# %% Convertir imagenes desde una ruta dada
+
+# Opcion 1
+# pages = convert_from_path(in_file)
+# pages[0].save(out_file, format="JPEG")
+
+# Opcion 2
+# pages = convert_from_path(in_file)
 # [page.save(out_file, "JPEG") for page in pages]
 
+# Opcion 3
+# pages = convert_from_path(in_file)
 # with BytesIO() as f:
 #     pages[0].save(f, format="jpeg")
 #     f.seek(0)
 #     img_page = Image.open(f)
 
+# Opcion 4
+# pages = convert_from_bytes(pdf_file=open(in_file, 'rb').read())
 
-#%%
 
-image = cv2.imread(out_file)
+# %% Pasar las imagenes a OpenCv
+
+# Opcion 1
+# image = cv2.imread(out_file)
+
+# Opcion 2
 # image = Image.open(out_file)
 
-# %%
+# Opcion 3
+# image_new = np.asarray(pages[0])
 
+# %% Definir la escala en funcion del alto o del ancho
+
+# Opcion con alto
 # alto_fijo = 1024
 # alto_porcentaje = (alto_fijo / float(image.size[1]))
 # ancho_objetivo = int(float(image.size[0])* float(alto_porcentaje))
-# image_new = image.resize((ancho_objetivo, alto_fijo))
-# image_new.save("hh.jpg")
 
+# Opcion con ancho
+# ancho_fijo = 768
+# alto, ancho, canales = image_new.shape
+# ancho_porcentaje = (ancho_fijo / ancho)
+# alto_objetivo = int(float(alto) * float(ancho_porcentaje))
 
-ancho_fijo = 768
-ancho_porcentaje = (ancho_fijo / float(image.shape[1]))
-alto_objetivo = int(float(image.shape[0]) * float(ancho_porcentaje))
+# %% Hacer la rescala de la imagen
 
-image_new = cv2.resize(image, (ancho_fijo, alto_objetivo))
+# Opcion 1
 # image_new = image.resize((ancho_fijo, alto_objetivo))
+
+# Opcion 2
+# image_new = cv2.resize(image_new, (ancho_fijo, alto_objetivo))
+
+# %% Salvar la imagen
+
+# Opcion 1
 # image_new.save("hh1.jpg")
 
-# %%
-
-cv2.imwrite(out_file, image_new)
-# %%
+# Opcion 2
+# cv2.imwrite(out_file, image_new)
--- vision/e2_extraccion/inicio.py
+++ vision/e2_extraccion/inicio.py
@@ -1,40 +1,60 @@
 
 # usar de pillow im.verify() para hacer una verificacion de imagen no corrompida
+# os.path.basename('data/5.pdf')
 
 #%%
 import os
 import imghdr
+from convert_pdf import pdf_a_imagen
 
-
-#%%
 # creacion de variables globales
 
-CARPETA = os.path.join('data')
+CARPETA = 'data'
 
+def recorre_carpeta(folder):
+    """Recorre una carpeta y regresa un generador para usar de uno en uno
 
-def valida_archivo(file):
+    Args:
+        folder ([type]): [description]
+    """
+
+    for (path, dirs, files) in os.walk(folder):
+        for file in files:
+            yield folder, file
+
+def valida_archivo(folder, file):
     """Funcion que valida el tipo de archivo
     regresa si es pdf, imagen jpeg, otro tipo de imagen, o archivo erroneo
 
     Args:
-        file ([type]): [description]
+        file ([str]): Archivo con la ruta completa
     """
 
-    a = imghdr.what(file)
+    a = imghdr.what(os.path.join(folder, file))
 
     if file.lower().endswith(('pdf')):
+        pdf_a_imagen(folder=folder, file=file)
         return print('archivo pdf')
     elif str(a) in ('jpeg'):
         return print('archivo sigue derecho es jpeg')
     elif a != None:
         return print('cambiar formato al archivo')
     else:
-        return print('archivo no se puede procesar formato no es jpeg o pdf')
+        return print('archivo {} no se puede procesar formato no es jpeg o pdf'.format(file))
 
+def ejecuta(folder):
 
-for (path, dirs, files) in os.walk(CARPETA):
-    for file in files:
-        valida_archivo(os.path.join(CARPETA, file))
+    _ = recorre_carpeta(folder)
+
+    while True:
+        try:
+            _folder, _file = next(_)
+            valida_archivo(_folder, _file)
+        except StopIteration:
+            print('fin del lote')
+            break
+
+ejecuta(CARPETA)
 
 
 # %%
